40feed832bdd9c2bf886e55ad114e1199f5fcc5e
achie27 <architmathur2011@gmail.com>
2017-05-19 01:16:55 +0530

    yiv game now starts based on the score obtained in the quiz

--- javascript.js
+++ javascript.js
@@ -5,26 +5,31 @@ var activityCounter = 0;
 var breakPoints = [];
 var showingActivity = false;
 var source;
+
+sessionStorage.setItem('score', -1);
 init();
+
 function init() {
-    for(var i= 0; i < 10; i++) {
+    for(var i= 0; i < 7; i++) {
         images[i] = "img"+i+".jpg";
     }
     activities[0] = "quiz1.html";
     activities[1] = "https://www.yiv.com/games/Elementary-Arithmetic-Game/index.html";
     activities[2] = "http://ekalavya.it.iitb.ac.in/summerinternship2017/activity_src/kBPlGvSIwN_gWulybVy6oQ/F49Nlw0i4lNn82dGASrsgA/Experiment/index.jsp";
 
-    breakPoints.push(2);
-    breakPoints.push(4);
-    breakPoints.push(6);
+    breakPoints.push(1);
 }
 
 function loaded() {
     source = document.getElementById("frame");
-    console.log(source);
 }
 
 function onClickNext() {
+	var theScore=parseInt(sessionStorage.getItem('score'));
+	if(theScore!=-1){
+    	breakPoints.push(theScore%3+2);
+    	sessionStorage.setItem('score', -1);
+    }
     if(!showingActivity && breakPoints.indexOf(imageCounter) != -1) {
         showingActivity = true;
         source.src = activities[activityCounter];
